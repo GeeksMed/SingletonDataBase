d86678f7c10d1dfbe98f99d26196a065cf2f4256
GeeksMed <silva.germain@gmail.com>
2023-03-04 09:34:38 -0300

    Commit Singleton DataBase Java - Teste

--- src/test/java/singleton/db/SingletonDataBaseTest.java
+++ src/test/java/singleton/db/SingletonDataBaseTest.java
@@ -1,9 +1,28 @@
 package singleton.db;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+
+import static junit.framework.Assert.assertEquals;
 
 public class SingletonDataBaseTest {
-    
+    @Test
+    public void deveRetornarRoute() {
+        SingletonDataBase.getInstance().setRoute("teste");
+        assertEquals("teste", SingletonDataBase.getInstance().getRoute());
+    }
+    @Test
+    public void deveRetornarPort() {
+        SingletonDataBase.getInstance().setPort(8181);
+        assertEquals(Integer.valueOf(8181), SingletonDataBase.getInstance().getPort());
+    }
+    @Test
+    public void deveRetornarUser() {
+        SingletonDataBase.getInstance().setUser("administrator");
+        assertEquals("administrator", SingletonDataBase.getInstance().getUser());
+    }
+    @Test
+    public void deveRetornarPassword() {
+        SingletonDataBase.getInstance().setPassword("M@sterkey");
+        assertEquals("M@sterkey", SingletonDataBase.getInstance().getPassword());
+    }
 }
